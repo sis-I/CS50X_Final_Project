09316c871f194ad1bc6ca2b230846839d24cb31d
sis-I <sisshash@gmail.com>
2025-01-09 08:18:18 +0300

    2.preparing app.py to use postgresql and query models

--- app.py
+++ app.py
@@ -39,76 +39,77 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy()
 
 class Dictionary(db.Model):
-  __tablename__ = 'dictionary1'
+    __tablename__ = 'dictionary1'
 
-  id = db.Column(db.Integer, primary_key=True)
-  amharic = db.Column(db.String(200))
-  english = db.Column(db.TEXT)
-  wordtype = db.Column(db.String(10))
-  reference = db.Column(db.String(20))
+    id = db.Column(db.Integer, primary_key=True)
+    amharic = db.Column(db.String(200))
+    english = db.Column(db.TEXT)
+    wordtype = db.Column(db.String(10))
+    reference = db.Column(db.String(20))
 
-  def __init__(sele, amharic, english, wordtype=None, reference=None):
-    self.amharic = amharic
-    self.english = english
-    self.wordtype = wordtype
-    self.reference = reference
+    def __init__(sele, amharic, english, wordtype=None, reference=None):
+        self.amharic = amharic
+        self.english = english
+        self.wordtype = wordtype
+        self.reference = reference
 
-  def serialize(self):
-    return {
-      'id': self.id,
-      'amharic': self.amharic,
-      'english': self.english,
-      'wordtype': self.wordtype,
-      'reference': self.reference
-    }
+    def serialize(self):
+        return {
+        'id': self.id,
+        'amharic': self.amharic,
+        'english': self.english,
+        'wordtype': self.wordtype,
+        'reference': self.reference
+        }
 
 
-  def __repr__(self):
-    return f"<Dictionary {self.amharic}>"
+    def __repr__(self):
+        return f"<Dictionary {self.amharic}>"
 
 
 class Bookmark(db.Model):
-  __tablename__ = 'bookmark1'
+    __tablename__ = 'bookmark1'
 
-  id = db.Column(db.Integer, primary_key=True)
-  # dictionary = db.relationship('Dictionary', backref='bookmark', lazy=True)
-  dict_id = db.Column(db.Integer, db.ForeignKey('dictionary1.id'))
-  date = db.Column(db.DateTime, default=datetime.utcnow)
+    id = db.Column(db.Integer, primary_key=True)
+    # dictionary = db.relationship('Dictionary', backref='bookmark', lazy=True)
+    dict_id = db.Column(db.Integer, db.ForeignKey('dictionary1.id'))
+    date = db.Column(db.DateTime, default=datetime.utcnow)
 
-  def __init__(self, dict_id):
-    self.dict_id = dict_id
+    def __init__(self, dict_id):
+        self.dict_id = dict_id
 
-  def serialize(self):
-    return {
-      'id': self.id,
-      'dict_id': self.dict_id,
-      'date': self.date
-    }
+    def serialize(self):
+        return {
+            'id': self.id,
+            'dict_id': self.dict_id,
+            'date': self.date
+        }
 
-  def __repr__(self):
-    return f"<Bookmark {self.dict_id}>"
+    def __repr__(self):
+        return f"<Bookmark {self.dict_id}>"
 
 
 class History(db.Model):
-  __tablename__ = 'history1'
+    __tablename__ = 'history1'
 
-  id = db.Column(db.Integer, primary_key=True)
-  # dictionary = db.relationship('Dictionary', backref='history', lazy=True)
-  dict_id = db.Column(db.Integer, db.ForeignKey('dictionary1.id'))
-  date = db.Column(db.DateTime, default=datetime.utcnow)
+    id = db.Column(db.Integer, primary_key=True)
+    # dictionary = db.relationship('Dictionary', backref='history', lazy=True)
+    dict_id = db.Column(db.Integer, db.ForeignKey('dictionary1.id'))
+    date = db.Column(db.DateTime, default=datetime.utcnow)
 
-  def __init__(self, dict_id):
-    self.dict_id = dict_id
+    def __init__(self, dict_id):
+        self.dict_id = dict_id
 
-  def serialize(self):
-    return {
-      'id': self.id,
-      'dict_id': self.dict_id,
-      'date': self.date
-    }
 
-  def __repr__(self):
-    return f"<History {self.dict_id}>"
+    def serialize(self):
+        return {
+            'id': self.id,
+            'dict_id': self.dict_id,
+            'date': self.date
+        }
+
+    def __repr__(self):
+        return f"<History {self.dict_id}>"
 
 
 # Initialize the database
@@ -139,7 +140,7 @@ def index():
             bk_rows.append(int(bm["dict_id"]))
 
     return render_template(
-        "index1.html",
+        "index.html",
         history_rows=h_rows,
         bookmark_rows=bk_rows,
     )
@@ -152,181 +153,175 @@ def search():
 
     if word:
         # Search for the word in the database
-        rows = Dictionary.query.filter(Dictionary.amharic.like(f"%{word}%")).all()
-        # rows = db.engine.execute(
-        #     "SELECT * FROM dictionary1 WHERE amharic= :word;",  {'word' : word}
-        # )
-        print(rows)
+        rows = db.session.execute(
+            db.select(Dictionary).where(Dictionary.amharic.like(f"%{word}%"))
+        ).scalars() #db.engine.execute("SELECT * FROM dictionary1 WHERE amharic LIKE ?;", f"%{word}%")
 
-        # rows = db.execute(
-        #     "SELECT * FROM dictionary WHERE amharic LIKE ?;", "%" + word + "%"
-        # )
         # If result found
         if rows is not None:
             # View search results
             return render_template("search.html", rows=rows)
 
     # For simplicity, just get 3 random words
-    rows = db.engine.execute("SELECT * FROM dictionary1 ORDER BY RANDOM() LIMIT 3;")
-
+    rows = db.session.execute(db.select(Dictionary).order_by(db.func.random()).limit(3)).scalars() #
+    # db.engine.execute("SELECT * FROM dictionary1 ORDER BY RANDOM() LIMIT 3;")
     return render_template("no-result.html", rows=rows)
 
 
-# @app.route("/dictionary/<word>")
-# def single_word(word):
-#     """View single word with its defination"""
+@app.route("/dictionary/<word>")
+def single_word(word):
+    """View single word with its defination"""
 
-#     word_rows = db.execute("SELECT * FROM dictionary WHERE amharic = ?;", word)
+    word_rows = db.execute("SELECT * FROM dictionary WHERE amharic = ?;", word)
 
-#     if word_rows:
-#         bookmark_rows = []
-#         if session.get("bookmark"):
-#             for bk in session["bookmark"]:
-#                 bookmark_rows.append(int(bk["dict_id"]))
-#         return render_template(
-#             "single-word.html", dict_word=word_rows[0], bookmark_rows=bookmark_rows
-#         )
-#     else:
-#         return "Word not Found!!"
-
-
-# @app.route("/recent-search")
-# def recent_search():
-#     """Recent word searched will be registered in history session"""
-#     dict_id = request.args.get("id")
-
-#     history_rows = session.get("history")
-#     if not history_rows:
-#         session.get("history").insert(0, {"dict_id": dict_id})
-
-#     # Check if current word id found in history
-#     else:
-#         found_in_history = False
-#         for hr in history_rows:
-#             if hr["dict_id"] == dict_id:
-#                 found_in_history = True
-#                 break
-
-#         if not found_in_history:
-#             session.get("history").insert(0, {"dict_id": dict_id})
-
-#     return redirect(url_for("index"))
+    if word_rows:
+        bookmark_rows = []
+        if session.get("bookmark"):
+            for bk in session["bookmark"]:
+                bookmark_rows.append(int(bk["dict_id"]))
+        return render_template(
+            "single-word.html", dict_word=word_rows[0], bookmark_rows=bookmark_rows
+        )
+    else:
+        return "Word not Found!!"
 
 
-# @app.route("/bookmark")
-# def bookmark():
-#     """Bookmark page: List of words in history"""
-#     bookmark_rows = session["bookmark"]
-#     rows = []
-#     if bookmark_rows:
-#         for br in bookmark_rows:
-#             row = db.execute("SELECT * FROM dictionary WHERE id = ?;", br["dict_id"])
-#             rows.append(row[0])
-#     return render_template("bookmark.html", bookmark=rows)
+@app.route("/recent-search")
+def recent_search():
+    """Recent word searched will be registered in history session"""
+    dict_id = request.args.get("id")
+
+    history_rows = session.get("history")
+    if not history_rows:
+        session.get("history").insert(0, {"dict_id": dict_id})
+
+    # Check if current word id found in history
+    else:
+        found_in_history = False
+        for hr in history_rows:
+            if hr["dict_id"] == dict_id:
+                found_in_history = True
+                break
+
+        if not found_in_history:
+            session.get("history").insert(0, {"dict_id": dict_id})
+
+    return redirect(url_for("index"))
 
 
-# @app.route("/history")
-# def history():
-#     """History page: List of words in history"""
-#     history_rows = session["history"]
-#     rows = []
-#     if history_rows:
-#         for hr in history_rows:
-#             row = db.execute("SELECT * FROM dictionary WHERE id = ?;", hr["dict_id"])
-#             rows.append(row[0])
-
-#     return render_template("history.html", history=rows)
-
-
-# @app.route("/add-or-delete-bookmark", methods=["POST"])
-# def add_or_delete_bookmark():
-#     """Toggle (add or remove) bookmark"""
-#     if request.method == "POST":
-#         req = request.get_json()
-#         bookmark_rows = session.get("bookmark")
-#         dict_id = req.get("id")
-#         res = make_response(jsonify(req), 200)
-
-#         if len(bookmark_rows) == 0:
-#             session["bookmark"].insert(0, {"dict_id": dict_id})
-
-#         else:
-#             is_bookmarked = False
-
-#             for bm in bookmark_rows:
-#                 if bm["dict_id"] == dict_id:
-#                     is_bookmarked = True
-#                     session["bookmark"].remove(bm)
-#                     break
-
-#             if not is_bookmarked:
-#                 session["bookmark"].insert(0, {"dict_id": dict_id})
-
-#         return res
+@app.route("/bookmark")
+def bookmark():
+    """Bookmark page: List of words in history"""
+    bookmark_rows = session["bookmark"]
+    rows = []
+    if bookmark_rows:
+        for br in bookmark_rows:
+            row = db.execute("SELECT * FROM dictionary WHERE id = ?;", br["dict_id"])
+            rows.append(row[0])
+    return render_template("bookmark.html", bookmark=rows)
 
 
-# @app.route("/bookmark/<id>/delete", methods=["POST"])
-# def delete_bookmark(id):
-#     """Delete Bookmark"""
-#     if request.method == "POST":
-#         bookmark_rows = session.get("bookmark")
-#         if len(bookmark_rows) > 0:
-#             for bk in bookmark_rows:
-#                 if bk["dict_id"] == id:
-#                     session["bookmark"].remove(bk)
-#                     break
+@app.route("/history")
+def history():
+    """History page: List of words in history"""
+    history_rows = session["history"]
+    rows = []
+    if history_rows:
+        for hr in history_rows:
+            row = db.execute("SELECT * FROM dictionary WHERE id = ?;", hr["dict_id"])
+            rows.append(row[0])
 
-#     return redirect("/bookmark")
-
-
-# @app.route("/bookmark/clear")
-# def clear_all_bookmark():
-#     # Clear all bookmark from session
-#     session.get("bookmark").clear()
-#     return redirect("/")
+    return render_template("history.html", history=rows)
 
 
-# @app.route("/delete-history", methods=["POST"])
-# def remove_history():
-#     """Delete single word in history"""
-#     if request.method == "POST":
-#         req = request.get_json()
+@app.route("/add-or-delete-bookmark", methods=["POST"])
+def add_or_delete_bookmark():
+    """Toggle (add or remove) bookmark"""
+    if request.method == "POST":
+        req = request.get_json()
+        bookmark_rows = session.get("bookmark")
+        dict_id = req.get("id")
+        res = make_response(jsonify(req), 200)
 
-#         print(req)
-#         histories = session.get("history")
-#         if histories:
-#             for h in histories:
-#                 if h["dict_id"] == req["id"]:
-#                     session["history"].remove(h)
-#                     break
+        if len(bookmark_rows) == 0:
+            session["bookmark"].insert(0, {"dict_id": dict_id})
 
-#         res = make_response(jsonify(req), 200)
+        else:
+            is_bookmarked = False
 
-#         return res
+            for bm in bookmark_rows:
+                if bm["dict_id"] == dict_id:
+                    is_bookmarked = True
+                    session["bookmark"].remove(bm)
+                    break
 
+            if not is_bookmarked:
+                session["bookmark"].insert(0, {"dict_id": dict_id})
 
-# @app.route("/history/<id>/delete", methods=["POST"])
-# def delete_history(id):
-#     """Delete history"""
-#     if request.method == "POST":
-#         history_rows = session.get("history")
-#         if len(history_rows) > 0:
-#             for h in history_rows:
-#                 if h["dict_id"] == id:
-#                     session["history"].remove(h)
-#                     break
-
-#     return redirect("/history")
+        return res
 
 
-# @app.route("/history/clear")
-# def clear_all_histroy():
-#     # Clear all history stored in the session
-#     session.get("history").clear()
-#     return redirect("/")
+@app.route("/bookmark/<id>/delete", methods=["POST"])
+def delete_bookmark(id):
+    """Delete Bookmark"""
+    if request.method == "POST":
+        bookmark_rows = session.get("bookmark")
+        if len(bookmark_rows) > 0:
+            for bk in bookmark_rows:
+                if bk["dict_id"] == id:
+                    session["bookmark"].remove(bk)
+                    break
+
+    return redirect("/bookmark")
+
+
+@app.route("/bookmark/clear")
+def clear_all_bookmark():
+    # Clear all bookmark from session
+    session.get("bookmark").clear()
+    return redirect("/")
+
+
+@app.route("/delete-history", methods=["POST"])
+def remove_history():
+    """Delete single word in history"""
+    if request.method == "POST":
+        req = request.get_json()
+
+        print(req)
+        histories = session.get("history")
+        if histories:
+            for h in histories:
+                if h["dict_id"] == req["id"]:
+                    session["history"].remove(h)
+                    break
+
+        res = make_response(jsonify(req), 200)
+
+        return res
+
+
+@app.route("/history/<id>/delete", methods=["POST"])
+def delete_history(id):
+    """Delete history"""
+    if request.method == "POST":
+        history_rows = session.get("history")
+        if len(history_rows) > 0:
+            for h in history_rows:
+                if h["dict_id"] == id:
+                    session["history"].remove(h)
+                    break
+
+    return redirect("/history")
+
+
+@app.route("/history/clear")
+def clear_all_histroy():
+    # Clear all history stored in the session
+    session.get("history").clear()
+    return redirect("/")
 
 
 if __name__ == "__main__":
-    # psycopg2.connect(os.environ.get("DATABASE_URL"))
     print(os.environ.get("DATABASE_URL"))
     app.run(debug=True)
