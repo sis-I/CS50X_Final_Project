668c51d8340d3a1e34c55d957aee9021122fc4ae
sis-I <sisshash@gmail.com>
2025-01-18 23:07:22 +0300

    add secret key

--- app.py
+++ app.py
@@ -22,11 +22,13 @@ load_dotenv()
 # Configure application
 app = Flask(__name__)
 
-# Configure session to use filesystem (instead of signed cookies)
-# app.config["SESSION_PERMANENT"] = False
-# app.config["SESSION_TYPE"] = "filesystem"
+app.secret_key = 'sdfdsfwe'
 
-# Session(app)
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+
+Session(app)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://neondb_owner:joaK7L8ybSNE@ep-curly-sky-a26pjcpp.eu-central-1.aws.neon.tech/neondb?sslmode=require"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -167,7 +169,7 @@ def recent_search():
     #     if not found_in_history:
     #         session.get("history").insert(0, {"dict_id": dict_id})
 
-    return redirect("/")
+    return redirect(url_for('index'))
 
 
 @app.route("/bookmark")
