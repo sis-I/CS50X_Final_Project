64352177690680c2a287b365c128de8d50a8d0d7
sis-I <sisshash@gmail.com>
2025-01-18 08:29:00 +0300

    modified sqlalchemy to 1.3.7 in requirements.txt

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 
 Session(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL") 
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL") 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 # SQLAlchemy database
