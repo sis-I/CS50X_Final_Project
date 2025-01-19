b9ee0d65981827754dd71d7477a0928cbd743056
sis-I <sisshash@gmail.com>
2025-01-19 23:27:57 +0300

    database url change key to 'DATABASE_URL_UNPOOLED'

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
 import os
 import uuid
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KY') or str(uuid.uuid4())
 
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or 'postgresql://neondb_owner:joaK7L8ybSNE@ep-curly-sky-a26pjcpp.eu-central-1.aws.neon.tech/neondb?sslmode=require'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL_UNPOOLED') or 'postgresql://neondb_owner:joaK7L8ybSNE@ep-curly-sky-a26pjcpp.eu-central-1.aws.neon.tech/neondb?sslmode=require'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Session configuration
