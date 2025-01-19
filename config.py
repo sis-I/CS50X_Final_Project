6ac7c0e86f33d69a55b289ceedffa82803531292
sis-I <sisshash@gmail.com>
2025-01-19 18:36:34 +0300

    correct database uri on config.py

--- config.py
+++ config.py
@@ -3,7 +3,10 @@ import uuid
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KY') or str(uuid.uuid4())
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or 'postgresql://posgres:password@localhost:port/db_name'
+
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or 'postgresql://neondb_owner:joaK7L8ybSNE@ep-curly-sky-a26pjcpp.eu-central-1.aws.neon.tech/neondb?sslmode=require'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Session configuration
     SESSION_TYPE = 'filesystem'
     SESSION_PERMANENT = False
